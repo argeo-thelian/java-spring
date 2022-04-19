487057e5d7c4c521c0d4cd321584d03fd0db815c
argeo-thelian <argeo.thelian@gmail.com>
2022-04-19 18:55:59 -0500

    add id product

--- service-product/src/main/java/com/arthe/store/product/controller/ProductController.java
+++ service-product/src/main/java/com/arthe/store/product/controller/ProductController.java
@@ -1,13 +1,13 @@
 package com.arthe.store.product.controller;
 
+import com.arthe.store.product.entity.Category;
 import com.arthe.store.product.entity.Product;
 import com.arthe.store.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,13 +16,33 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-    @GetMapping
-    public ResponseEntity<List<Product>> listProduct(){
-        List<Product> products = productService.listAllProduct();
 
-        if (products.isEmpty()){
-            return ResponseEntity.noContent().build();
+    @GetMapping
+    public ResponseEntity<List<Product>> listProduct(
+            @RequestParam( name = "categoryId", required = false) Long categoryId
+    )
+    {
+        List<Product> products = new ArrayList<>();
+        if (null == categoryId){
+            products = productService.listAllProduct();
+            if (products.isEmpty()){
+                return ResponseEntity.noContent().build();
+            }
+        }else{
+            products = productService.findByCategory(Category.builder().id(categoryId).build());
+            if (products.isEmpty()){
+                return ResponseEntity.notFound().build();
+            }
         }
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id){
+        Product product = productService.getProduct(id);
+        if (null == product){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
     }
 }
